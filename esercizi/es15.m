aa025507d8f3a78ba8eba9c6ff0b478f0a9f4921
Shot <michaelcavicchioli.informatica@gmail.com>
2023-05-19 06:15:45 +0200

    Add grafici

--- esercizi/es15.m
+++ esercizi/es15.m
@@ -7,9 +7,9 @@ Q = 2*eye(n) + diag(ones(n-1,1),1) + diag(ones(n-1,1),-1);
 func = @(x) 1/2*x'*Q*x-e'*(sin((pi/2)*x)+x);
 grad  = @(x) Q*x - e' * (1 + cos((pi/2)*x));
 jacob = @(x) Q + e' * (sin((pi/2) * x));
-%tol = 1e-3;
+tol = 1e-3;
 %tol = 1e-8;
-tol = 1e-13;
+%tol = 1e-13;
 
 %fprintf([repmat('%.2f\t', 1, size(e, 2)) '\n'], e');
 %fprintf([repmat('%.2f\t', 1, size(Q, 2)) '\n'], Q');
@@ -18,9 +18,7 @@ tol = 1e-13;
 
 [x, nit] = newton_j(grad, jacob, zeros(n,1), tol, 1000);
 
-%fprintf([repmat('%.2f\t', 1, size(x, 2)) '\n'], x');
-%fprintf([repmat('%.2f\t', 1, size(nit, 2)) '\n'], x');
-%plot(x, 1:1:100);
-%plot(1:1:100, x);
-
-writematrix(x, "terzo.txt")
+fprintf([repmat('%.2f\t', 1, size(x, 2)) '\n'], x');
+fprintf([repmat('%.2f\t', 1, size(nit, 2)) '\n'], x');
+plot(x, 1:1:100);
+plot(1:1:100, x);
